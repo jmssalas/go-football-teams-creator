292e0357c9d703f6226eb8be28f71f610f814ca3
jmssalas <hola@jmssalas.com>
2026-09-21 07:19:32 +0200

    Add noCacheMiddleware

--- main.go
+++ main.go
@@ -15,10 +15,10 @@ func main() {
 
 	r := api.Router(&dbAdapter)
 
-	r.StaticFS("/css", http.Dir("./web/css"))
-	r.StaticFS("/js", http.Dir("./web/js"))
-	r.StaticFS("/assets", http.Dir("./web/assets"))
-	r.StaticFile("/", "./web/index.html")
+	r.StaticFS("/css", http.Dir("./web/css")).Use(noCacheMiddleware())
+	r.StaticFS("/js", http.Dir("./web/js")).Use(noCacheMiddleware())
+	r.StaticFS("/assets", http.Dir("./web/assets")).Use(noCacheMiddleware())
+	r.StaticFile("/", "./web/index.html").Use(noCacheMiddleware())
 	r.NoRoute(func(c *gin.Context) {
 		if c.Request.Method == http.MethodGet {
 			c.File("./web/index.html")
@@ -31,4 +31,13 @@ func main() {
 	http.ListenAndServe(":8080", r)
 
 	dbAdapter.CloseDb()
+}
+
+func noCacheMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Header("Cache-Control", "no-cache, no-store, must-revalidate")
+		c.Header("Pragma", "no-cache")
+		c.Header("Expires", "0")
+		c.Next()
+	}
 }
